ec627d5837954cf704d151c80eccef2d2d2a5b53
Arpit Bajpai <ab1790197@gmail.com>
2026-08-23 22:38:54 +0530

    Time: 7 ms (79.56%), Space: 47.1 MB (41.61%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,43 +1,28 @@
 class Solution {
+
     public boolean sumGame(String num) {
-
         int n = num.length();
+        int[] left = get(num.substring(0, n / 2));
+        int[] right = get(num.substring(n / 2, n));
 
-        int leftSum = 0;
-        int rightSum = 0;
+        int n0 = left[0],
+            q0 = left[1];
+        int n1 = right[0],
+            q1 = right[1];
 
-        int leftQ = 0;
-        int rightQ = 0;
+        return (q0 + q1) % 2 == 1 || n0 - n1 != ((q1 - q0) * 9) / 2;
+    }
 
-        // Left half
-        for (int i = 0; i < n / 2; i++) {
-            char c = num.charAt(i);
-
-            if (c == '?') {
-                leftQ++;
+    private int[] get(String s) {
+        int nn = 0,
+            qq = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '?') {
+                qq++;
             } else {
-                leftSum += c - '0';
+                nn += ch - '0';
             }
         }
-
-        // Right half
-        for (int i = n / 2; i < n; i++) {
-            char c = num.charAt(i);
-
-            if (c == '?') {
-                rightQ++;
-            } else {
-                rightSum += c - '0';
-            }
-        }
-
-        // Odd number of '?' -> Alice wins
-        if ((leftQ + rightQ) % 2 == 1) {
-            return true;
-        }
-
-        // Otherwise check whether Bob can make sums equal
-        return leftSum - rightSum !=
-               9 * (rightQ - leftQ) / 2;
+        return new int[] { nn, qq };
     }
 }
